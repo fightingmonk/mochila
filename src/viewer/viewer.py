f031cdcf8b022163f1bdc87941d05efe40ad4eb3
jonathan <jonathan@tappister.com>
2023-05-19 13:08:30 -0700

    feat: Connect curated viewer to arangodb

--- src/viewer/viewer.py
+++ src/viewer/viewer.py
@@ -1,26 +1,21 @@
-import solara
+import solara, json
+from utils import get_db_connection
+
+arango_db = get_db_connection()
 
 # Declare reactive variables at the top level. Components using these variables
 # will be re-executed when their values change.
-sentence = solara.reactive("Solara makes our team more productive.")
-word_limit = solara.reactive(10)
+db_name = ''
+collections = None
 
 
 @solara.component
 def Page():
-    # Calculate word_count within the component to ensure re-execution when reactive variables change.
-    word_count = len(sentence.value.split())
+    db_name = arango_db.db_name
+    collections = json.dumps([col for col in arango_db.collections() if col['name'][0] != '_'], indent=2)
 
-    solara.SliderInt("Word limit", value=word_limit, min=2, max=20)
-    solara.InputText(label="Your sentence", value=sentence, continuous_update=True)
-
-    # Display messages based on the current word count and word limit.
-    if word_count >= int(word_limit.value):
-        solara.Error(f"With {word_count} words, you passed the word limit of {word_limit.value}.")
-    elif word_count >= int(0.8 * word_limit.value):
-        solara.Warning(f"With {word_count} words, you are close to the word limit of {word_limit.value}.")
-    else:
-        solara.Success("Great short writing!")
+    solara.Text(db_name)
+    solara.HTML('pre', collections)
 
 
 # The following line is required only when running the code in a Jupyter notebook:
